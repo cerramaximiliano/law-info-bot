eec9bfabfbb1402033abb8be217ac6391beb425c
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-08-23 07:56:28 -0300

    fix/ cron jobs

--- src/index.js
+++ src/index.js
@@ -23,16 +23,23 @@ const logger = require("./config/logger");
 // Conectar a MongoDB
 connectDB();
 
-cron.schedule("00 9-17 * * 1-5", async () => {
-  try {
-    logger.info("Tarea de envío de mensajes de bot iniciada");
-    const unnotified = await notifyUnnotifiedNews(); // Llama a la función para notificar noticias no notificadas
-    const unnotifiedActs = await notifyUnnotifiedNews("acts", 10, 2);
-    logger.info("Tarea de envío de mensajes de bot finalizada");
-  } catch (err) {
-    logger.error(`Error en tarea de envío de mensajes: ${err}`);
+cron.schedule(
+  "00 9-17 * * 1-5",
+  async () => {
+    try {
+      logger.info("Tarea de envío de mensajes de bot iniciada");
+      const unnotified = await notifyUnnotifiedNews(); // Llama a la función para notificar noticias no notificadas
+      const unnotifiedActs = await notifyUnnotifiedNews("acts", 10, 2);
+      logger.info("Tarea de envío de mensajes de bot finalizada");
+    } catch (err) {
+      logger.error(`Error en tarea de envío de mensajes: ${err}`);
+    }
+  },
+  {
+    scheduled: true,
+    timezone: "America/Argentina/Buenos_Aires",
   }
-});
+);
 
 cron.schedule("*/15 * * * *", async () => {
   try {
@@ -42,7 +49,7 @@ cron.schedule("*/15 * * * *", async () => {
     const hammurabiNews = await scrapeHammurabi();
     logger.info("Tarea de web scraping finalizada");
   } catch (err) {
-    logger.error("Error en tarea de web scraping");
+    logger.error("Error en tarea de web scraping:", err);
   }
 });
 
@@ -63,6 +70,7 @@ cron.schedule(
   }
 );
 
-(async () => {
+/* (async () => {
   await scrapeGPCourses();
 })();
+ */
