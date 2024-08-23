ac512d6dfbdd2ac56f0750d37e116e5b6ecdd3fe
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-08-23 19:30:42 -0300

    fix/ notify courses

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ dotenv.config({ path: envFile });
 const connectDB = require("./config/db");
 const { startCronJobs } = require("./services/cronJobs");
 const logger = require("./config/logger");
+const { notifyUpcomingCourses } = require("./controllers/telegramBotControllers");
 
 // Conectar a MongoDB
 connectDB();
 
 // Iniciar tareas programadas
 startCronJobs();
-
 
 logger.info("Aplicación iniciada correctamente");
